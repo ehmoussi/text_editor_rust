afd374f6a0f90caff07f21c40392a21bd00a54f1
El Hadi Moussi <moussi@phimeca.com>
2024-11-23 18:31:54 +0100

    Add enable_raw_mode function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use std::io::{self, Read};
 
+use libc;
+use termios::*;
+
+fn enable_raw_mode() -> io::Result<()> {
+    let fd = libc::STDIN_FILENO;
+    let mut raw = Termios::from_fd(fd)?;
+    let _ = tcgetattr(fd, &mut raw);
+    raw.c_lflag &= !(libc::ECHO);
+    let _ = tcsetattr(fd, libc::TCSAFLUSH, &mut raw);
+    Ok(())
+}
+
 fn main() -> () {
+    let _ = enable_raw_mode();
     let mut buffer = [0; 1];
     loop {
         let _ = io::stdin().read(&mut buffer[..]);
