acff2ea138d7788a80506e92433694474c6254ee
El Hadi Moussi <moussi@phimeca.com>
2024-11-23 18:48:38 +0100

    Add a way to disable raw mode

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,17 @@ use std::io::{self, Read};
 use libc;
 use termios::*;
 
+fn create_termios() -> Termios {
+    let fd = libc::STDIN_FILENO;
+    return Termios::from_fd(fd).expect("Failed to create a termios struct");
+}
+
+fn disable_raw_mode(mut termios: Termios) -> io::Result<()> {
+    let fd = libc::STDIN_FILENO;
+    let _ = tcsetattr(fd, libc::TCSAFLUSH, &mut termios);
+    Ok(())
+}
+
 fn enable_raw_mode() -> io::Result<()> {
     let fd = libc::STDIN_FILENO;
     let mut raw = Termios::from_fd(fd)?;
@@ -13,6 +24,7 @@ fn enable_raw_mode() -> io::Result<()> {
 }
 
 fn main() -> () {
+    let orig_termios = create_termios();
     let _ = enable_raw_mode();
     let mut buffer = [0; 1];
     loop {
@@ -20,6 +32,7 @@ fn main() -> () {
         let c = buffer[0] as char;
         println!("{}", c);
         if c == 'q' {
+            let _ = disable_raw_mode(orig_termios);
             break;
         };
     }
