e597164fd3176add19330aba10a164659d3588aa
El Hadi Moussi <moussi@phimeca.com>
2024-11-23 18:15:28 +0100

    Read one charcter at a time and stop with the letter q

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use std::io;
+use std::io::{self, Read};
 
-fn main() {
-    let mut buffer = String::new();
-    let _ = io::stdin().read_line(&mut buffer);
+fn main() -> () {
+    let mut buffer = [0; 1];
+    loop {
+        let _ = io::stdin().read(&mut buffer[..]);
+        let c = buffer[0] as char;
+        println!("{}", c);
+        if c == 'q' {
+            break;
+        };
+    }
 }
